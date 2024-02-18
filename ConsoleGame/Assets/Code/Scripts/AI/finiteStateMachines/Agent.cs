38e16fbea37b9cd612a6dcf0e93a8524c2dd13f7
Kieronhall <kieron.hall2@mail.bcu.ac.uk>
2024-02-18 21:51:40 +0000

    Patrol Functionality added

--- ConsoleGame/Assets/Code/Scripts/AI/finiteStateMachines/Agent.cs
+++ ConsoleGame/Assets/Code/Scripts/AI/finiteStateMachines/Agent.cs
@@ -23,6 +23,15 @@ public class Agent : MonoBehaviour
     private float timer;
     private Vector3 startingPoint;
 
+    //Patrol Variables
+
+    public GameObject patrolFlag1;
+    public GameObject patrolFlag2;
+    public GameObject patrolFlag3;
+    public GameObject patrolFlag4;
+    private Transform currentGoal;
+
+
     public enum Type
     {
         Wandering,
@@ -44,14 +53,20 @@ public class Agent : MonoBehaviour
         s = this.gameObject.GetComponent<sensors>();
 
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-
-        //Wander
-        timer = wanderTimer;
-        startingPoint = transform.position;
-        SetNewRandomDestination();
+        
+        switch (agentType)
+        {
+            case Type.Wandering:
+                timer = wanderTimer;
+                startingPoint = transform.position;
+                SetNewRandomDestination();
+                break;
+            case Type.Patrol:
+                break;
+        }
     }
 
-    // SEEK PLAYER CODE
+    // SEEK CODE
     public void agentGoToPlayer()
     {
         agent.destination = player.position;
@@ -80,6 +95,48 @@ public class Agent : MonoBehaviour
             timer = wanderTimer;
         }
     }
+    // PATROL CODE
+    public void PatrolFunc()
+    {
+        if (agent == null)
+        {
+            Debug.LogError("Agent is null.");
+            return;
+        }
+
+        // Check if the agent has reached the current goal
+        if (!agent.pathPending && agent.remainingDistance < 0.1f)
+        {
+            SetNextGoal();
+        }
+    }
+    void SetNextGoal()
+    {
+        // Switch the goal to the next flag
+        if (currentGoal == patrolFlag1.transform)
+        {
+            currentGoal = patrolFlag2.transform;
+        }
+        else if (currentGoal == patrolFlag2.transform)
+        {
+            currentGoal = patrolFlag3.transform;
+        }
+        else if (currentGoal == patrolFlag3.transform)
+        {
+            currentGoal = patrolFlag4.transform;
+        }
+        else if (currentGoal == patrolFlag4.transform)
+        {
+            currentGoal = patrolFlag1.transform;
+        }
+        else
+        {
+            currentGoal = patrolFlag1.transform; // Default to patrolFlag1 if currentGoal is null or unexpected
+        }
+
+        // Set the destination for the NavMeshAgent
+        agent.SetDestination(currentGoal.position);
+    }
 
     // Update is called once per frame
     void Update()
@@ -100,6 +157,15 @@ public class Agent : MonoBehaviour
                 }
                 break;
             case Type.Patrol:
+                if (s.Hit && (sm.getCurrState().GetType() != typeof(SeekState)))
+                {
+                    Debug.Log("Hit ");
+                    sm.pushState(seek);
+                }
+                if ((s.Hit == false) && (sm.getCurrState().GetType() != typeof(PatrolState)))
+                {
+                    sm.pushState(patrol);
+                }
                 //if (s.Hit && (sm.getCurrState().GetType() != typeof(SeekState)) &&
                 //    (Time.time > lastSeekStateExitTime + seekStateCooldown))
                 //{
@@ -117,11 +183,11 @@ public class Agent : MonoBehaviour
 
 
 
-#if UNITY_EDITOR   
+#if UNITY_EDITOR
     
     void OnDrawGizmos()
     {
-        // Draw a wireframe sphere in the scene view to visualize the wander radius
+        // wander radius
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, wanderRadius);
 
@@ -140,6 +206,17 @@ public class Agent : MonoBehaviour
         else
         {
             Debug.LogWarning("state machine is null.");
+        }
+
+        // agent path
+        if (agent != null && agent.path != null)
+        {
+            // Draw the path in Gizmos
+            Gizmos.color = Color.blue;
+            for (int i = 0; i < agent.path.corners.Length - 1; i++)
+            {
+                Gizmos.DrawLine(agent.path.corners[i], agent.path.corners[i + 1]);
+            }
         }
     }
 #endif

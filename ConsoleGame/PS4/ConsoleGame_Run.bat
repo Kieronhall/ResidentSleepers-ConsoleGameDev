57d5632b7361577bb5388d441909ec56f54994fe
Kieronhall <kieron.hall2@mail.bcu.ac.uk>
2024-02-29 15:04:38 +0000

    Prototype Demonstration Commit
    Joao level ready to presentation

--- ConsoleGame/PS4/ConsoleGame_Run.bat
+++
@@ -1,4 +0,0 @@
-pushd "%~dp0"
-orbis-ctrl pkill
-orbis-run  /fsroot . /console:process /noprogress /log:"eboot.log" /elf "Build\eboot.self"
-popd
--- ConsoleGame/PS4/ConsoleGame_RunOnPS5.bat
+++
@@ -1,5 +0,0 @@
-pushd "%~dp0"
-set tmpath=%SCE_ROOT_DIR%\Prospero\Tools\Target Manager Server\bin
-"%tmpath%\prospero-ctrl.exe" process kill 2>NUL
-"%tmpath%\prospero-run.exe" /log:"eboot.log" /elf "Build\eboot.self"
-popd

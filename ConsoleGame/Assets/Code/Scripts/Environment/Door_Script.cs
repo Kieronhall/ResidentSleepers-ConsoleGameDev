2c9244ba8ba63d1dd4c439f42cc9fe445d63744e
rahman0002 <91261400+rahman0002@users.noreply.github.com>
2024-03-13 22:39:43 +0000

    animation blending for player from idle to aim

--- ConsoleGame/Assets/Code/Scripts/Environment/Door_Script.cs
+++ ConsoleGame/Assets/Code/Scripts/Environment/Door_Script.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using ThirdPerson;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -24,7 +25,6 @@ public class Door_Script : MonoBehaviour
     public float doorSpeed;
     public bool doorOpen=false;
 
-    // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");

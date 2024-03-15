938c171f0d392aae14ae3ad3e9256141be7c17a3
JoaoCodices <Joao.Codices@mail.bcu.ac.uk>
2024-03-15 16:37:45 +0000

    stupid changes

--- ConsoleGame/Assets/Code/Scripts/Environment/RoomDetection.cs
+++ ConsoleGame/Assets/Code/Scripts/Environment/RoomDetection.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomDetection : MonoBehaviour
@@ -7,12 +9,31 @@ public class RoomDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Floor0":
+                Debug.Log("Player In " + other.tag);
+                break;
+            case "Floor1":
+                Debug.Log("Player In " + other.tag);
+                break;
+            case "Floor2":
+                Debug.Log("Player In " + other.tag);
+                break;
+            case "Floor3":
+                Debug.Log("Player In " + other.tag);
+                break;
+        }
     }
 }

37bad72ede90894118fae4bad3b0b34a1f61327f
Kieronhall <kieron.hall2@mail.bcu.ac.uk>
2024-03-15 17:47:03 +0000

    Muzzle Flash shows when shooting

--- ConsoleGame/Assets/Code/Scripts/AI/finiteStateMachines/ShootingState.cs
+++ ConsoleGame/Assets/Code/Scripts/AI/finiteStateMachines/ShootingState.cs
@@ -19,14 +19,14 @@ public class ShootingState : State
         agent.animRunningOff();
         agent.animWalkingOff();
         agent.GunReposition(new Vector3 (-0.0102f, 0.0235f, -0.0356f), Quaternion.Euler(-3.768f, -150.63f, -181.568f), "gun");
+        agent.StartCoroutine(agent.muzzleFlashVisible());
     }
 
     public override void Execute()
     {
         Debug.Log("Executing Shooting");
         agent.StandStill();
-        agent.PopShooting();
-        //agent.agentAnimShoot();
+        agent.PopShooting(); //checks to pop or not
     }
 
     public override void Exit()
@@ -36,5 +36,6 @@ public class ShootingState : State
         agent.animShootingOff();
         agent.animRunningOn();
         agent.GunReposition(new Vector3(-0.0179f, 0.0269f, -0.0171f), Quaternion.Euler(-29.43f, -91.69f, -182f), "gun");
+        agent.muzzleFlashInvisible();
     }
 }

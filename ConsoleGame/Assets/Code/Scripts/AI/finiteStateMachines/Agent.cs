ecccf23756eb9c1bcb32f590b47190bd979d7ca8
Kieronhall <kieron.hall2@mail.bcu.ac.uk>
2024-05-08 14:13:13 +0100

    fsmAgent Audio fully implemented
    Shooting and Death audio implemented

--- ConsoleGame/Assets/Code/Scripts/AI/finiteStateMachines/Agent.cs
+++ ConsoleGame/Assets/Code/Scripts/AI/finiteStateMachines/Agent.cs
@@ -256,6 +256,7 @@ public class Agent : MonoBehaviour
     {
         agent.isStopped = true;
         muzzleFlash.SetActive(false);
+        agentShootingAudioStop();
         TurnOffUpdate();
     }
 

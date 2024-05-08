d52a274a3d9d408a99ade89d30a06a1f79e89a91
JoaoCodices <Joao.Codices@mail.bcu.ac.uk>
2024-05-08 15:00:59 +0100

    Squashed commit of the following:
    commit 26ae79712cb336c30f46ee5e203a1d7533ddc37c
    Author: JoaoCodices <Joao.Codices@mail.bcu.ac.uk>
    Date:   Wed May 8 15:00:27 2024 +0100
    
        Finished Level 1
    
    commit 4ff33a81c4279b6c05a74d8b57b9b9c8f5d8e90b
    Author: JoaoCodices <Joao.Codices@mail.bcu.ac.uk>
    Date:   Wed May 8 14:45:41 2024 +0100
    
        Changes to Level 1

--- ConsoleGame/Assets/Code/Scripts/Level_1 GameController/PlayerLvl1.cs
+++ ConsoleGame/Assets/Code/Scripts/Level_1 GameController/PlayerLvl1.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerLvl1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TMP_Text uiText;
+
     void Start()
     {
-        
+        uiText.text = "Infiltrate the Facility! Watchout for the Guards!";
     }
 
     // Update is called once per frame
@@ -22,5 +24,36 @@ public class PlayerLvl1 : MonoBehaviour
             Debug.Log("Exterior Disabled");
             GameObject.FindGameObjectWithTag("Exterior").SetActive(false);
         }
+        if (other.name=="Zone1")
+        {
+            Debug.Log("Zone1");
+            uiText.text = "Avoid being detected and locate the backdoor! Hide in the bins if you need!";
+        }
+        if (other.name == "Zone2")
+        {
+            Debug.Log("Zone2");
+            uiText.text = "The Backdoor is Locked! Locate the Eletric Panel!";
+        }
+        if (other.name == "Zone3")
+        {
+            Debug.Log("Zone3");
+            uiText.text = "Navigate the Ground Floor until you find the Laser Room!";
+        }
+        if (other.name == "Zone4")
+        {
+            Debug.Log("Zone4");
+            uiText.text = "The Laser Room! Avoid being hit and make it to the staircase door!";
+        }
+        if (other.name == "Zone5")
+        {
+            Debug.Log("Zone5");
+            uiText.text = "Get to the Top Floor and Disable Lockdown Mode!";
+        }
+        if (other.name == "Zone6")
+        {
+            Debug.Log("Zone6");
+            uiText.text = "Locate and Disable Lockdown! Also Find the Lift to the Cell Floors!";
+        }
+
     }
 }
